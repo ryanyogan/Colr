ba37ca7bc47b87786d2f7ee1af3ed2b16c9939fc
Ryan Yogan <ryanyogan@gmail.com>
2014-05-27 17:44:42 -0500

    Basic wiring up views to app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,17 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    # Declare our window object, use the full scren
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    # Controller initializations
+    @search_controller = SearchController.alloc.initWithNibName(nil, bundle: nil)
+    @navigation_controller = UINavigationController.alloc.initWithRootViewController @search_controller
+
+    # Set the window root view to the navigation controller
+    @window.rootViewController = @navigation_controller
+    @window.makeKeyAndVisible
+
     true
   end
 end
